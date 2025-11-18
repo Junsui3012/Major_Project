3bcc2deacc4f348f03c3f479075d8726d86f4bfb
Junsui3012 <sajals8909@gmail.com>
2025-11-18 22:02:11 +0530

    beautiful test.py

--- models/test.py
+++ models/test.py
@@ -1,16 +1,32 @@
 import pandas as pd
 import joblib
 import os
+import warnings
+# Make sure to download the requirements.txt in an appopriate virtual env .venv
 
+# Silent the warnings
+warnings.filterwarnings("ignore")
+
+# Make sure to put the model in the same directory as the calling script before running the code below
 dir = os.path.dirname(os.path.abspath(__file__))
 f_path= os.path.join(dir, "model.joblib")
 print(f_path)
 
+# Loading model object in a variable
 model = joblib.load(f_path)
 
-res = model.predict(pd.DataFrame({'Rainfall_mm' : [150], 
-                            'Temperature_Celsius' : [30], 
-                            'Fertilizer_Used' : [1], 
-                            'Irrigation_Used' : [1]
+# Calling predict method.
+res = model.predict(pd.DataFrame({'Rainfall_mm' : [150],        # 1st Feature is the average rainfall in past month in mm
+                            'Temperature_Celsius' : [30],       # 2nd Feature is the average temperature in past month in celsius
+                            'Fertilizer_Used' : [1],            # 3rd Feature is a 0 or 1 value if any fertilizer was used or not
+                            'Irrigation_Used' : [1]             # 4th Feature is a 0 or 1 value if any Irrigation was used or not
                             }))
-print(res)
+
+
+"""
+Alternate caliing method:
+res = model.predict([[150, 30, 1, 1]])
+"""
+
+# Returns an array of n results for n inputs
+print(res[0])
